ff81fd86fec1f7684a8c6d0f04a83e2bdd64fd16
AleksBulajic <aleks.bulajic25@gmail.com>
2024-06-29 12:35:35 -0400

    added questions for each exercasxercise

--- script.js
+++ script.js
@@ -58,11 +58,9 @@ Exercise 5: Insert an element between two others
 
 Complete Exercise 5 in the space below:
 */
-
 foods.splice(2, 0, 'tofu');
 
 console.log('Exercise 5 result:', foods);
-
 /*
 Exercise 6: Replace elements
 
@@ -179,6 +177,7 @@ Exercise 12: FizzBuzz with arrays
 
 Complete Exercise 12 in the space below:
 */
+
 
 const fizz = [];
 const buzz = [];
